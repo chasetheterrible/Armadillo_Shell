db2cf3a173bbe9af96063b8ec66e940b2c3b672f
chasetheterrible <156257514+chasetheterrible@users.noreply.github.com>
2025-11-24 15:15:32 -0700

    change directory

--- Commands/BuiltinCommands/ChangeDirectory/ChangeDirectory.h
+++ Commands/BuiltinCommands/ChangeDirectory/ChangeDirectory.h
@@ -12,33 +12,57 @@
 
 
 class ChangeDirectory : public Command<ChangeDirectory> { // Command class needs to be inherited in order to work!!!
-  vector<string> tokenizedCommand;
+  std::vector<std::string> tokenizedCommand;
   // add more class variables as needed.
 
 public:
-  explicit ChangeDirectory(vector<string>& tokens) {
+  explicit ChangeDirectory(std::vector<std::string>& tokens) {
     tokenizedCommand = tokens; // should save arguments in the order they were passed in
   }
 
-  static string returnManText() {
+  static std::string returnManText() {
     return ChangeDirectoryManual;
   }
 
-  static bool validateSyntax(vector<string>& tokens) {
+  static bool validateSyntax(std::vector<std::string>& tokens) {
     // TODO: implement
     // this should be a simple validation so it can be used when validating
     // commands that are getting piped. More thorough validations can be done
     // in the execute command itself.
     // tokens should contain all of the command inputs the user provided
     // in order. However, It will not contain the command at the start.
-    return true;
+    return toekns.size() <= 1;
   }
 
-  vector<string> executeCommand() override {
+  std::vector<std::string> executeCommand() override {
     // TODO: implement
     // Will assume validateSyntax was already called, but add error handling just in case
-    return {"Not Implemented", "500"};
-  }
+    if (!validateSyntax(tokenizedCommand)) {
+      return { "cd: too many areguments", "400"};
+    }
+
+    const char* targetPath = nullptr;
+
+    if (tokenizedCommand.empty()) {
+      targetPath = std::getenv("HOME");
+      if (!targetPath) {
+        return { "cd: HOME not set", "500" }
+      }
+    }
+    else {
+      targetPath = tokenizedCommand[0].c_str();
+    }
+
+
+    if (chdir(targetPath) != 0) {
+      if (!tokenizedCommand.empty()) {
+        return { "cd: not such file or directory:", + tokenizedCommand[0], "404"};
+      } else {
+        return { "cd: failed to change directory", "500"}; 
+      }
+    }
+
+  
 
 private:
   // put your own method definitions here
